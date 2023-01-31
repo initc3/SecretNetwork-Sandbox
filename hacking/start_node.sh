10a354cd6d85455a92a01fa2d393fe55825ee48d
njeans <nerlajeanlous@gmail.com>
2023-01-31 15:06:00 -0600

    working simple demo

--- hacking/start_node.sh
+++ hacking/start_node.sh
@@ -22,25 +22,22 @@ docker-compose up localsecret-2 -d
 progs=$(docker-compose exec localsecret-2 ps -ef)
 while [[ "$progs" != *"secretd start --rpc.laddr tcp://0.0.0.0:26657"* ]] 
 do 
+    echo "Waiting for secretd build and node start..."
     progs=$(docker-compose exec localsecret-2 ps -ef)
     ./logs.sh    
-    echo "Waiting for secretd build and node start..."
     sleep 5
 
 done
 
-#waiting for blocks to start being produced before turning of localsecret-1
+#waiting for blocks to start being produced before turning off localsecret-1
 logs=$(docker-compose exec localsecret-2 cat /root/out )
-while [[ "$logs" != *"finalizing commit of block"* ]] 
+while [[ "$logs" != *"executed block"* ]] 
 do 
+    echo "Waiting for blocks to be produced..."
     logs=$(docker-compose exec localsecret-2 cat /root/out )
     ./logs.sh 
-    echo "Waiting for blocks to be produced..."
     sleep 5
 done
 
-docker-compose stop localsecret-1
+# docker-compose stop localsecret-1
 ./logs.sh
-
-docker cp hacking-localsecret-2-1:/usr/bin/secretd ./secretd
-docker cp hacking-localsecret-2-1:/root/io-master-cert.der ./io-master-cert.der
