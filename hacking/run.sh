7ae25ad5b76ff0e9411839368786988a52f5659a
lilione <lilioneviola@gmail.com>
2023-02-02 14:07:51 -0600

    (yunqi) refactor code for mev demo

--- hacking/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-#docker-compose stop localsecret-1
-
-docker-compose exec localsecret-2 ./scripts/test_simple.sh
-# docker-compose exec localsecret-2 cp -rf /root/hist_data  /root/.secretd/data/ 
-# ./rebuild_node.sh &
-# docker-compose start localsecret-1
--- hacking/scripts/startup.sh
+++ hacking/scripts/startup.sh
@@ -5,7 +5,6 @@ if [ "$SECRET_NODE_TYPE" == "NODE" ]
 then
   echo "startup {NODE} node"
   ./scripts/node_init.sh
-  # sleep infinity
 else
   echo "startup {BOOTSTRAP} node"
   ./scripts/bootstrap_init.sh
