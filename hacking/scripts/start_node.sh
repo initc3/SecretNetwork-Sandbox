c2047519c3768b0df4399d9cc86d6f3f24c1c5da
Sylvain Bellemare <sbellem@gmail.com>
2023-02-02 15:41:06 -0600

    Hacking updates
    * ignore CONTRACT_ADDRESS file
    * remove cosmos_sdk mount from hacking/Dockerfile

--- hacking/scripts/start_node.sh
+++ hacking/scripts/start_node.sh
@@ -3,42 +3,42 @@
 set -e
 set -x
 
-docker-compose down
+docker compose down
 
-docker-compose up localsecret-1 -d
+docker compose up localsecret-1 -d
 sleep 5
 
-genesis=$(docker-compose exec localsecret-1 ls /genesis)
+genesis=$(docker compose exec localsecret-1 ls /genesis)
 while [[ "$genesis" != *"genesis.json"* ]] 
 do 
     echo "Waiting for gensis file to be generated..."
-    genesis=$(docker-compose exec localsecret-1 ls /genesis)
-    docker-compose logs localsecret-1 --tail 10   
+    genesis=$(docker compose exec localsecret-1 ls /genesis)
+    docker compose logs localsecret-1 --tail 10   
     sleep 5
 done
 
-docker-compose up localsecret-2 -d
+docker compose up localsecret-2 -d
 
 #waiting to build secretd and start node
-progs=$(docker-compose exec localsecret-2 ps -ef)
+progs=$(docker compose exec localsecret-2 ps -ef)
 while [[ "$progs" != *"secretd start --rpc.laddr tcp://0.0.0.0:26657"* ]] 
 do 
     echo "Waiting for secretd build and node start..."
-    progs=$(docker-compose exec localsecret-2 ps -ef)
-    docker-compose logs localsecret-2 --tail 10   
+    progs=$(docker compose exec localsecret-2 ps -ef)
+    docker compose logs localsecret-2 --tail 10   
     sleep 5
 done
 
 #waiting for blocks to start being produced before turning off localsecret-1
-logs=$(docker-compose logs localsecret-2 --tail 10)
+logs=$(docker compose logs localsecret-2 --tail 10)
 while [[ "$logs" != *"executed block"* ]] 
 do 
     echo "Waiting for blocks to be produced..."
-    logs=$(docker-compose logs localsecret-2 --tail 10)
-    docker-compose logs localsecret-2 --tail 10   
+    logs=$(docker compose logs localsecret-2 --tail 10)
+    docker compose logs localsecret-2 --tail 10   
     sleep 5
 done
 
-docker-compose logs localsecret-2 --tail 10
+docker compose logs localsecret-2 --tail 10
 
 ./scripts/setup.sh
