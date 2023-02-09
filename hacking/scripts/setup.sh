669248db2fb1dbd8831f0bb7c9a26ab5adab4f47
Sylvain Bellemare <sbellem@gmail.com>
2023-02-09 01:49:15 -0600

    artifacts: wip

--- hacking/scripts/setup.sh
+++ hacking/scripts/setup.sh
@@ -8,3 +8,4 @@ docker compose exec localsecret-2 ./scripts/setup_snip20.sh
 #docker-compose exec localsecret-2 ./debug_scripts/set_init_states_simple.sh
 
 docker compose stop localsecret-1
+docker compose logs localsecret-2 --follow
