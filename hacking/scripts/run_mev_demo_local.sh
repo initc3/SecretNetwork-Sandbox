efb875bb66d475ceb576983910bfd2ff8a755f17
lilione <lilioneviola@gmail.com>
2023-08-21 16:27:56 -0500

    benchmark script

--- hacking/scripts/run_mev_demo_local.sh
+++ hacking/scripts/run_mev_demo_local.sh
@@ -21,23 +21,82 @@ cnt=0
 lo=0
 hi=100
 while [ $(expr $hi - $lo) -ne 1 ]; do
+  ###
+  start_loop=$(date +%s.%N)
+  ###
+
+  ###
+  start=$(date +%s.%N)
+  ###
   set_snapshot "${UNIQUE_LABEL}-${cnt}"
+  ###
+  end=$(date +%s.%N)
+  time_diff=$(echo "$end - $start" | bc)
+  echo "-----------------------------------------------------------set_snapshot: $time_diff ms"
+  ###
 
   mid=$((($hi + $lo) / 2))
   echo "lo:$lo hi:$hi mid:$mid"
 
+  ###
+  start=$(date +%s.%N)
+  ###
   generate_and_sign_swap token_a $mid 0 $ADV adv
+  ###
+  end=$(date +%s.%N)
+  time_diff=$(echo "$end - $start" | bc)
+  echo "-----------------------------------------------------------gen and sign adv tx: $time_diff ms"
+  ###
   echo "adv tx token_a $mid 0"
 
+  ###
+  start=$(date +%s.%N)
+  ###
   simulate_tx adv
+  ###
+  end=$(date +%s.%N)
+  time_diff=$(echo "$end - $start" | bc)
+  echo "-----------------------------------------------------------simulate adv tx: $time_diff ms"
+  ###
+  ###
+  start=$(date +%s.%N)
+  ###
   old_pool_a=$(query_pool pool_a)
+  ###
+  end=$(date +%s.%N)
+  time_diff=$(echo "$end - $start" | bc)
+  echo "-----------------------------------------------------------query old pool: $time_diff ms"
+  ###
+  ###
+  start=$(date +%s.%N)
+  ###
   simulate_tx victim
+  ###
+  end=$(date +%s.%N)
+  time_diff=$(echo "$end - $start" | bc)
+  echo "-----------------------------------------------------------simulate victim tx: $time_diff ms"
+  ###
+  ###
+  start=$(date +%s.%N)
+  ###
   new_pool_a=$(query_pool pool_a)
+  ###
+  end=$(date +%s.%N)
+  time_diff=$(echo "$end - $start" | bc)
+  echo "-----------------------------------------------------------query new pool: $time_diff ms"
+  ###
   dif_pool_a=$(($new_pool_a - $old_pool_a))
   echo 'dif' $dif_pool_a
 
   if [ $dif_pool_a -gt 0 ]; then lo=$mid; else hi=$mid; fi
   cnt=$((cnt + 1))
+
+  ###
+  end_loop=$(date +%s.%N)
+  time_diff=$(echo "$end_loop - $start_loop" | bc)
+  echo "-----------------------------------------------------------single loop: $time_diff ms"
+  ###
+
   echo "=================================== end of a trial"
   echo
 done
