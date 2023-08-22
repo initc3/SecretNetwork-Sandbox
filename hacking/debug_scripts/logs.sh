35357bfada9988f7f6d212871439794bf4b9a1e2
Sylvain Bellemare <sbellem@gmail.com>
2023-08-22 16:53:02 -0400

    Remove hacking dir

--- hacking/debug_scripts/logs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose exec localsecret-2 cat /root/out
--- hacking/scripts/local_test_params.sh
+++
@@ -1,4 +0,0 @@
-ADMIN="secret1fc3fzy78ttp0lwuujw7e52rhspxn8uj52zfyne"
-UNIQUE_LABEL=$(date "+%Y-%m-%d-%H:%M:%S")
-SECRETD=secretd
-CHAIN_ID="secretdev-1"
