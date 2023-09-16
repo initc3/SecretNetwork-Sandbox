c27514b2da7dbba623a1b4355251eff360ae5d03
Sylvain Bellemare <sbellem@gmail.com>
2023-09-16 02:20:30 -0700

    Add benchmark for trusted vs untrusted code

--- hacking/scripts/run_mev_demo_local.sh
+++ hacking/scripts/run_mev_demo_local.sh
@@ -3,6 +3,10 @@ set -e
 
 source ./scripts/mev_utils.sh
 
+rm -f /tmp/ecall_handle_millis.log
+rm -f /tmp/ecall_handle_micros.log
+rm -f /tmp/ecall_handle_nanos.log
+
 CONTRACT_ADDRESS=`cat $CONTRACT_LOC/contractAddress.txt`
 CODE_HASH=`cat $CONTRACT_LOC/codeHash.txt`
 
@@ -17,21 +21,25 @@ generate_and_sign_swap token_a 10 20 $VICTIM victim
 echo "victim tx amount of token A = 10, expected return = 20"
 echo
 
+# use the nano benchmark logs to measure the loop only
+rm -f /tmp/ecall_handle_nanos.log
+
+start_time=$(date +%s%N)
 cnt=0
 lo=0
 hi=100
 while [ $(expr $hi - $lo) -ne 1 ]; do
   ###
-  start_loop=$(date +%s.%N)
+  start_loop=$(date +%s%N)
+  ###
+  ###
+  start=$(date +%s%N)
   ###
 
-  ###
-  start=$(date +%s.%N)
-  ###
   set_snapshot "${UNIQUE_LABEL}-${cnt}"
   ###
-  end=$(date +%s.%N)
-  time_diff=$(echo "$end - $start" | bc)
+  end=$(date +%s%N)
+  time_diff=$(echo "($end - $start)/1000000" | bc)
   echo "-----------------------------------------------------------set_snapshot: $time_diff ms"
   ###
 
@@ -39,50 +47,50 @@ while [ $(expr $hi - $lo) -ne 1 ]; do
   echo "lo:$lo hi:$hi mid:$mid"
 
   ###
-  start=$(date +%s.%N)
+  start=$(date +%s%N)
   ###
   generate_and_sign_swap token_a $mid 0 $ADV adv
   ###
-  end=$(date +%s.%N)
-  time_diff=$(echo "$end - $start" | bc)
+  end=$(date +%s%N)
+  time_diff=$(echo "($end - $start)/1000000" | bc)
   echo "-----------------------------------------------------------generate and sign adversary transaction: $time_diff ms"
   ###
   echo "adv tx token_a $mid 0"
 
   ###
-  start=$(date +%s.%N)
+  start=$(date +%s%N)
   ###
   simulate_tx adv
   ###
-  end=$(date +%s.%N)
-  time_diff=$(echo "$end - $start" | bc)
+  end=$(date +%s%N)
+  time_diff=$(echo "($end - $start)/1000000" | bc)
   echo "-----------------------------------------------------------simulate adversary transaction: $time_diff ms"
   ###
   ###
-  start=$(date +%s.%N)
+  start=$(date +%s%N)
   ###
   old_pool_a=$(query_pool pool_a)
   ###
-  end=$(date +%s.%N)
-  time_diff=$(echo "$end - $start" | bc)
+  end=$(date +%s%N)
+  time_diff=$(echo "($end - $start)/1000000" | bc)
   echo "-----------------------------------------------------------query old pool: $time_diff ms"
   ###
   ###
-  start=$(date +%s.%N)
+  start=$(date +%s%N)
   ###
   simulate_tx victim
   ###
-  end=$(date +%s.%N)
-  time_diff=$(echo "$end - $start" | bc)
+  end=$(date +%s%N)
+  time_diff=$(echo "($end - $start)/1000000" | bc)
   echo "-----------------------------------------------------------simulate victim transaction: $time_diff ms"
   ###
   ###
-  start=$(date +%s.%N)
+  start=$(date +%s%N)
   ###
   new_pool_a=$(query_pool pool_a)
   ###
-  end=$(date +%s.%N)
-  time_diff=$(echo "$end - $start" | bc)
+  end=$(date +%s%N)
+  time_diff=$(echo "($end - $start)/1000000" | bc)
   echo "-----------------------------------------------------------query new pool: $time_diff ms"
   ###
   dif_pool_a=$(($new_pool_a - $old_pool_a))
@@ -92,14 +100,32 @@ while [ $(expr $hi - $lo) -ne 1 ]; do
   cnt=$((cnt + 1))
 
   ###
-  end_loop=$(date +%s.%N)
-  time_diff=$(echo "$end_loop - $start_loop" | bc)
+  end_loop=$(date +%s%N)
+  time_diff=$(echo "($end_loop - $start_loop)/1000000" | bc)
   echo "-----------------------------------------------------------single loop: $time_diff ms"
   ###
 
   echo "=================================== end of a trial"
   echo
 done
+
+end_time=$(date +%s%N)
+
+ecall_time_nanos=$(awk '{ sum += $1 } END { print sum }' /tmp/ecall_handle_nanos.log)
+ecall_time_millis=$( echo "$ecall_time_nanos / 1000000" | bc)
+
+total_time_nanos=$(echo "$end_time - $start_time" | bc)
+total_time_millis=$(echo "$total_time_nanos / 1000000" | bc)
+
+untrusted_time_nanos=$(echo "$total_time_nanos - $ecall_time_nanos" | bc)
+untrusted_time_millis=$(echo "$total_time_millis - $ecall_time_millis" | bc)
+
+printf "\nTotal time spent for trial: ${total_time_nanos} nanosecs"
+printf "\nEstimated time spent for (trusted) ecall_handle ${ecall_time_nanos} nanosecs"
+printf "\nEstimated time spent for untrusted code ${untrusted_time_nanos} nanosecs\n"
+printf "\nTotal time spent for trial: ${total_time_millis} millisecs"
+printf "\nEstimated time spent for (trusted) ecall_handle ${ecall_time_millis} millisecs"
+printf "\nEstimated time spent for untrusted code ${untrusted_time_millis} millisecs\n\n"
 
 set_snapshot "${UNIQUE_LABEL}-${cnt}"
 echo "final front-run tx token_a $lo 0"
